847406de127403e20adfe06f8708fd349454c2a2
Abdullah Jaffer <abdullahjaffer7@gmail.com>
2023-10-24 22:30:47 +0500

    feat: added new api call to BE

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+require("dotenv/config");
 
 // Server Initialization
 const app = express();
@@ -8,7 +9,22 @@ const PORT = 3001;
 app.use(express.json());
 
 // Routes will be written here
-app.use("/news", (req, res) => {});
+app.get("/news", (req, res) => {
+	const apiKey = process.env.NYC_API_KEY;
+	const apiUrl = `https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=${apiKey}`;
+	fetch(apiUrl)
+		.then((response) => response.json())
+		.then((response) => {
+			res.send(response);
+		})
+		.catch((error) => {
+			res.status(500).send("Error fetching data from the New York Times API");
+		});
+});
+
+app.use("*", (req, res) => {
+	res.status(404).send("Not found");
+});
 
 // Server Listen Along with Database
 // connection(in case of data persistence)
